b6b853e0afca733e04d018a3854260c6a5897a46
AFPedreros <felipe.pedreros94@gmail.com>
2022-10-24 14:29:24 -0500

    refactoring the code

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import products from "./data.js"
 let totalPrice = 0
 const orderContainer = document.getElementById("order-container")
 
+//* Render the products cards and add the event listener for the shop buttons
 const render = () => {
     renderProducts()
     addShopEvents()
@@ -32,12 +33,6 @@ const renderProducts = () => {
     document.getElementById("product-gallery").innerHTML = productsHtml
 }
 
-const renderOrder = () => {
-    totalPrice > 0
-        ? orderContainer.classList.add("show")
-        : orderContainer.classList.remove("show")
-}
-
 const addShopEvents = () => {
     for (let product of products) {
         const btn = document.getElementById(`${product.id}-shop-btn`)
@@ -48,19 +43,14 @@ const addShopEvents = () => {
     }
 }
 
-const addRemoveEvent = (product) => {
-    const btn = document.getElementById(`${product.id}-remove-btn`)
-    btn.addEventListener("click", () => {
-        document.getElementById(`${product.id}-order-product`).remove()
-        document.getElementById(`${product.id}-shop-btn`).disabled = false
-
-        totalPrice -= product.price
-        document.getElementById("total-price").innerText = `$${totalPrice}.00`
-
-        renderOrder()
-    })
+// Helper function for show or hide the order section
+const renderOrder = () => {
+    totalPrice > 0
+        ? orderContainer.classList.add("show")
+        : orderContainer.classList.remove("show")
 }
 
+//* Adds the product to the order list and the events listener for the remove buttons
 const addProductToOrder = (product) => {
     let div = document.createElement("div")
     div.className = "order-product"
@@ -78,6 +68,19 @@ const addProductToOrder = (product) => {
 
     addRemoveEvent(product)
     renderOrder()
+}
+
+const addRemoveEvent = (product) => {
+    const btn = document.getElementById(`${product.id}-remove-btn`)
+    btn.addEventListener("click", () => {
+        document.getElementById(`${product.id}-order-product`).remove()
+        document.getElementById(`${product.id}-shop-btn`).disabled = false
+
+        totalPrice -= product.price
+        document.getElementById("total-price").innerText = `$${totalPrice}.00`
+
+        renderOrder()
+    })
 }
 
 document.getElementById("order-complete-btn").addEventListener("click", () => {
