74f65277348bf4e0da46be66b602de88db7573e1
AFPedreros <felipe.pedreros94@gmail.com>
2022-10-30 17:05:54 -0500

    fixing the images

--- data.js
+++ data.js
@@ -1,23 +1,27 @@
+import { earrings } from "./images/topos-colombia.jpg"
+import { necklace } from "./images/collar-colombia.jpg"
+import { bookmark } from "./images/separador-mariposa.jpg"
+
 const products = [
     {
         name: "Colombia necklace",
         id: 0,
         price: 147,
-        img: "images/collar-colombia.jpg",
+        img: necklace,
         alt: "Collar de colombia",
     },
     {
         name: "Butterfly bookmark",
         id: 1,
         price: 159,
-        img: "images/separador mariposa.jpg",
+        img: bookmark,
         alt: "Separador de mariposa",
     },
     {
         name: "Colombia earrings",
         id: 2,
         price: 139,
-        img: "images/topos-colombia.jpg",
+        img: earrings,
         alt: "Topos de Colombia",
     },
 ]
